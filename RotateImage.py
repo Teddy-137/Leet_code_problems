d7004096f5d59932e56e8fd7445befe22b11f7df
Teddy-137 <Tewodrosanteneh@outlook.com>
2024-11-13 19:22:08 +0300

    Create RotateImage.py
    This code rotates a given matrix 90 degree clockwise

---
+++ RotateImage.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rotate(self, matrix):
+        n = len(matrix)
+        # transpose
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        
+        #vertical reflection
+
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j],matrix[i][n - j - 1] = matrix[i][n - j - 1],matrix[i][j]
+        
